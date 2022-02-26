11f1b95012b8c16e89694fe861ca377da1738a44
Mohamed ABID <mabid@e2r12p17.clusters.42paris.fr>
2022-02-26 19:39:51 +0100

    legere avancer sur comment fonctionne push-swap

--- test.h
+++ test.h
@@ -17,5 +17,8 @@ typedef struct s_list
 int     ft_lstsize(t_list *lst);
 t_list *ft_lstnew(void *content);
 void    ft_lstdelone(t_list *lst, void (*del)(void *));
+void    ft_lstadd_front(t_list **alst, t_list *new);
+void    ft_lstadd_back(t_list **alst, t_list *new);
+t_list  *ft_lstlast(t_list *lst);
 
 #endif
--- tst.c
+++ tst.c
@@ -1,5 +1,14 @@
 #include "./test.h"
-
+/**
+ * @brief Alloue (avec malloc(3)) et renvoie un nouvel
+élément. La variable membre ’content’ est
+initialisée à l’aide de la valeur du paramètre
+’content’. La variable ’next’ est initialisée à
+NULL.
+ * 
+ * @param content 
+ * @return t_list* 
+ */
 t_list  *ft_lstnew(void *content)
 {
         t_list  *one;
@@ -22,6 +31,12 @@ t_list  *ft_lstnew(void *content)
         return (one);
 }
 
+/**
+ * @brief Ajoute l’élément ’new’ au début de la liste.
+ * 
+ * @param lst 
+ * @return int 
+ */
 int     ft_lstsize(t_list *lst)
 {
         int     len;
@@ -36,6 +51,14 @@ int     ft_lstsize(t_list *lst)
         return (len);
 }
 
+/**
+ * @brief Libère la mémoire de l’élément passé en argument en
+utilisant la fonction ’del’ puis avec free(3). La
+mémoire de ’next’ ne doit pas être free.
+ * 
+ * @param lst 
+ * @param del 
+ */
 void    ft_lstdelone(t_list *lst, void (*del)(void *))
 {
         if (!del)
@@ -51,14 +74,86 @@ void    ft_lstdelone(t_list *lst, void (*del)(void *))
         }
 }
 
+/**
+ * @brief Ajoute l’élément ’new’ au début de la liste.
+ * 
+ * @param alst 
+ * @param new 
+ */
+void    ft_lstadd_front(t_list **alst, t_list *new)
+{
+        t_list  *tmp;
+
+        if (!alst)
+                return ;
+
+        if (*alst)
+        {
+                printf("Valeur de tmp 1 :%p\n",tmp);
+                tmp = *alst;
+                printf("Valeur de alst :%p\n", alst);
+                *alst = new;
+                printf("Valeur de new :%p\n", new);
+                new->next = tmp;
+                printf("Valeur de tmp 2 :%p\n",tmp);
+        }
+        else
+        {
+                *alst = new;
+                printf("Else :%p\n",new);
+        }
+}
+
+/**
+ * @brief Renvoie le dernier élément de la liste.
+ * 
+ * @param lst 
+ * @return t_list* 
+ */
+t_list  *ft_lstlast(t_list *lst)
+{
+        while (lst && lst->next != NULL)
+        {
+                lst = lst->next;
+                printf("%p\n",lst);
+        }
+        return (lst);
+}
+
+/**
+ * @brief Ajoute l’élément ’new’ à la fin de la liste.
+ * 
+ * @param alst 
+ * @param new 
+ */
+void    ft_lstadd_back(t_list **alst, t_list *new)
+{
+        if (*alst)
+        {
+                ft_lstlast(*alst)->next = new;
+                printf("If *alst alors new :%p\n",new);
+        }
+        else
+        {
+                printf("Sinon alst :%p\n",*alst);
+                *alst = new;
+                printf("Puis new :%p\n",new);
+        }
+}
+
+
 int main()
 {
     t_list one;
     t_list *two;
+    t_list **three;
     void *del;
 
     ft_lstnew(one.element);
-    ft_lstsize(ft_lstnew(one.element));
-    ft_lstdelone(two,del);
+    //ft_lstdelone(two,del);
+    //ft_lstadd_front(three,two);
+    //ft_lstlast(two);
+    //ft_lstadd_back(three, two);
+    //ft_lstsize(two);
     return(0);
 }
